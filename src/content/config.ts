6198a342b584ca5e05658d2119e80731049869df
benyalf <joselinalfaro71@gmail.com>
2024-02-22 23:27:06 -0500

    Update Astro Collections Config

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,6 +1,7 @@
 import { defineCollection, z } from 'astro:content';
 
-const blog = defineCollection({
+
+const postsCollection = defineCollection({
 	type: 'content',
 	// Type-check frontmatter using a schema
 	schema: z.object({
@@ -13,4 +14,21 @@ const blog = defineCollection({
 	}),
 });
 
-export const collections = { blog };
+const servicesCollection = defineCollection({
+	type: 'content',
+	// Type-check frontmatter using a schema
+	schema: z.object({
+		title: z.string(),
+		description: z.string(),
+		// Transform string to Date object
+		pubDate: z.coerce.date(),
+		updatedDate: z.coerce.date().optional(),
+		heroImage: z.string().optional(),
+	}),
+});
+
+
+export const collections = {
+	posts: postsCollection,
+	services: servicesCollection,
+};
